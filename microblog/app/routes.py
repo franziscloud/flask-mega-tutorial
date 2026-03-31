220b096966b7035ec31faf53b677a2f5ab5cf5ac
franziscloud <franziscloud@gmail.com>
2026-03-31 21:47:03 +0800

    Chapter 2: Template Inheritence

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,6 +1,22 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, Franz"
+    user = {'username': 'Franz'}
+    posts = [
+        { 
+            'author': {'username': 'Czarina'},
+            'body': 'Beautiful day in Rizal!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Ready or Not Part 2 Movie was so scary'
+        },
+        {
+            'author': {'username': 'Franz'},
+            'body': 'The 1Q84 is a long read'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
